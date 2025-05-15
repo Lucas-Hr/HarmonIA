84949e44eb9f8efd23ed8ffe55cc03c76953fcf8
Lucas-Hr <lucashenintsoarandriamiarisoa@gmail.com>
2025-05-15 08:50:40 +0300

    feat : download file

--- app/ui/accueil/converter/result.tsx
+++ app/ui/accueil/converter/result.tsx
@@ -1,14 +1,28 @@
-import { File } from "buffer";
+// import { File } from "buffer";
 import Image from "next/image";
 import { useCallback } from "react";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-export default function Result() {
-    
-     const download = () => {
+type ResultProps = {
+     file : any;
+}
 
+export default function Result({file} : ResultProps) {
+    
+     const download = (f : any) => {
+          if (!f) return;
+
+          const url = URL.createObjectURL(f); // Create a blob URL for the File
+          const link = document.createElement("a");
+          link.href = url;
+          link.download = f.name; // Suggested filename
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          URL.revokeObjectURL(url); // Free memory
      }
+
     return (
            <motion.div 
            className="flex flex-col items-center ms-4"
@@ -19,8 +33,8 @@ export default function Result() {
            }}
            >
                 <Image src="/sheet.png" width={300} height={500} alt="sheet"/>
-                <p className="mt-2">File name.pdf</p>
-                <button className="bg-white text-black px-5 py-2 rounded-lg mt-4 cursor-pointer w-full flex justify-center hover:bg-[#DCDCDC]" onClick={() => download()}><Image src="/download.svg" width={25} height={25} alt="download"/>Telecharger</button>
+                <p className="mt-2">{file?.name}</p>
+                <button className="bg-white text-black px-5 py-2 rounded-lg mt-4 cursor-pointer w-full flex justify-center hover:bg-[#DCDCDC]" onClick={() => download(file)}><Image src="/download.svg" width={25} height={25} alt="download"/>Telecharger</button>
            </motion.div>
         )
 }
