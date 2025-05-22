0a6dd845191d91a590cebecdfc8600552ce337ae
Lucas-Hr <lucashenintsoarandriamiarisoa@gmail.com>
2025-05-22 07:56:25 +0300

    Adding Ispm Logo

--- app/ui/accueil/navbar.tsx
+++ app/ui/accueil/navbar.tsx
@@ -20,8 +20,8 @@ export default function Navbar() {
     return (
         <div className="flex items-center p-5 text-white border-b-2 border-[#404040] bg-[#070707] fixed w-full px-40 justify-between z-4 relative">
             <div className="flex ">
-                {/* <Image src='/ispm.png' width={50} height={20} alt="ispm" /> */}
-                <h1 className={`text-3xl ${ericaOne.className}`}>HarmonIA</h1>
+                <Image src='/logo_ispm.png' width={50} height={20} alt="ispm" />
+                <h1 className={` ms-4 text-3xl ${ericaOne.className}`}>HarmonIA</h1>
             </div>
             <div className="ml-5 text-base flex">
                     {links.map((link) => {
